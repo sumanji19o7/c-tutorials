e6a8d6dc4f0c3cdcad8a2e8eb61aaea60cf289aa
sumanji19o7 <shiven106@gmail.com>
2025-10-02 09:52:05 +0530

    for and while loop done

---
+++ loops.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//for loop
+
+int main(){
+    for (int i=1;i<=5;i+=1){
+    printf("%d\n",i);
+    }
+    int a=1;
+
+    //while loop
+    while(a<=5){
+        printf("%d\n",a);
+        a+=1;
+        
+        //do while loop in book
+        
+    }
+    return 0;
+
+
+}
+
